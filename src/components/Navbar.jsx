e3f13bb6d58e835f8c8b562f0d6e1eb148c9bdf9
Nashit <nashitquddus1@gmail.com>
2023-10-31 16:42:42 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  Menu as MenuIcon,
+  Search,
+  SettingsOutlined,
+  ArrorDropDownOutlined,
+} from "@mui/icons-material";
+import FlexBetween from "./FlexBetween";
+import { useDispatch } from "react-redux";
+import { setMode } from "state";
+import { useTheme } from "@emotion/react";
+import { AppBar, Toolbar } from "@mui/material";
+// import profileImage from
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const theme = useTheme();
+  return (
+    <AppBar
+      sx={{
+        position: "static",
+        background: "none",
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar sx={{ justifyContent: "space-between" }}></Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
