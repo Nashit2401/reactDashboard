24e95bcf47b1e98d7f25bfb58b719e3e3bc9086b
Nashit Quddus <nashitquddus1@gmail.com>
2023-11-02 01:55:33 -0700

    sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,10 @@ import FlexBetween from "./FlexBetween";
 import { useDispatch } from "react-redux";
 import { setMode } from "state";
 import { useTheme } from "@emotion/react";
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, IconButton, InputBase, Menu, Toolbar } from "@mui/material";
 // import profileImage from
 
-const Navbar = () => {
+const Navbar = ({ isSidebarOpen, setIsSidebarOpen }) => {
   const dispatch = useDispatch();
   const theme = useTheme();
   return (
@@ -25,7 +25,43 @@ const Navbar = () => {
         boxShadow: "none",
       }}
     >
-      <Toolbar sx={{ justifyContent: "space-between" }}></Toolbar>
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        {/* left side */}
+        <FlexBetween>
+          <IconButton
+            onClick={() => {
+              setIsSidebarOpen(!isSidebarOpen);
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <FlexBetween
+            backgroundColor={theme.palette.background.alt}
+            borderRadius="9px"
+            gap={"3rem"}
+            padding={"0.1rem 1.5rem"}
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        </FlexBetween>
+
+        {/* Right Side */}
+        <FlexBetween>
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkModeOutlined sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightModeOutlined sx={{ fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <IconButton>
+            <SettingsOutlined sx={{ fontSize: "25px" }} />
+          </IconButton>
+        </FlexBetween>
+      </Toolbar>
     </AppBar>
   );
 };
