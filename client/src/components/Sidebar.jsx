5b32fd1e4476a99a15a2d4c60f0eac991d036056
Nashit Quddus <nashitquddus1@gmail.com>
2023-11-08 03:37:52 -0800

    added prod screen

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -10,6 +10,7 @@ import {
   Typography,
   useTheme,
   ListItemText,
+  Divider,
 } from "@mui/material";
 import {
   ChevronLeft,
@@ -25,10 +26,12 @@ import {
   TrendingUpOutlined,
   PieChartOutline,
   CalendarMonthOutlined,
+  SettingsOutlined,
 } from "@mui/icons-material";
 import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import FlexBetween from "./FlexBetween";
+import profileImage from "../images/img1.jpg";
 
 const navItems = [
   {
@@ -90,6 +93,7 @@ const navItems = [
 ];
 
 const Sidebar = ({
+  user,
   drawerWidth,
   isSidebarOpen,
   setIsSidebarOpen,
@@ -189,6 +193,42 @@ const Sidebar = ({
                 );
               })}
             </List>
+          </Box>
+          <Box position={"absolute"} bottom={"2rem"}>
+            <Divider />{" "}
+            <FlexBetween
+              textTransform={"none"}
+              gap={"1rem"}
+              m={"1.5rem 2rem 0rem 3rem"}
+            >
+              <Box
+                component={"img"}
+                alt="'profile"
+                src={profileImage}
+                height={"40px"}
+                width={"40px"}
+                borderRadius={"50%"}
+                sx={{ objectFit: "cover" }}
+              />
+              <Box textAlign={"left"}>
+                <Typography
+                  fontWeight={"bold"}
+                  fontSize={"0.9rem"}
+                  sx={{ color: theme.palette.secondary[100] }}
+                >
+                  {user.name}
+                </Typography>
+                <Typography
+                  fontSize={"0.8rem"}
+                  sx={{ color: theme.palette.secondary[200] }}
+                >
+                  {user.occupation}
+                </Typography>
+              </Box>
+              <SettingsOutlined
+                sx={{ color: theme.palette.secondary[300], fontSize: "25px" }}
+              />
+            </FlexBetween>
           </Box>
         </Drawer>
       )}
